06598a6f963deb385d46b9aa7ffd25a5d6f7cb5b
Peter Karman <karpet@peknet.com>
2013-01-14 00:12:11 -0600

    init

---
+++ lib/Dezi/Bot.pm
@@ -0,0 +1,74 @@
+package Dezi::Bot;
+use warnings;
+use strict;
+
+our $VERSION = '0.001'
+
+=head1 NAME
+
+Dezi::Bot - web crawler
+
+=head1 SYNOPSIS
+
+ use Dezi::Bot;
+
+=cut
+
+1;
+
+__END__
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Peter Karman, C<< <karman at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dezi-bot at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dezi-Bot>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dezi::Bot
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dezi-Bot>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dezi-Bot>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dezi-Bot>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dezi-Bot/>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Peter Karman.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
